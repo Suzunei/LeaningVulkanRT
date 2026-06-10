8ad93af78a37125c7bc7a82a75d74ee5f1160826
qilin <1601690147@qq.com>
2026-06-10 15:30:28 +0800

    First Commit

---
+++ VulkanRT/VulkanRT/Window.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "WindowConfig.hpp"
+#include "Vulkan.hpp"
+#include <functional>
+#include <vector>
+
+namespace Vulkan
+{
+	class Window final 
+	{
+	public:
+
+		VULKAN_NON_COPIABLE(Window)
+
+		explicit Window(const WindowConfig& config);
+		~Window();
+
+		//window instance properties
+		const WindowConfig& Config() const { return config_; }
+		GLFWwindow* Handle() const { return window_; }
+		float ContentScale() const;
+		VkExtent2D FramebufferSize() const;
+		VkExtent2D WindowSize() const;
+
+		//GLFW instance properties
+		const char* GetKeyName(int key, int scancode) const;
+		std::vector<const char*> GetRequiredInstanceExtensions() const;
+		double GetTime() const;
+
+		//Callbacks
+		std::function<void()> DrawFrame;
+
+
+	private:
+
+		const WindowConfig config_;
+		GLFWwindow* window_{};
+	};
+}

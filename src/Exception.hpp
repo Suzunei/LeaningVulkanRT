ae0df19ecdbffdbefd8cedf66f391bd1fcab0cff
Suzunei <1601690147@qq.com>
2026-06-14 13:02:11 +0800

    VulkanRT: Init window

--- src/Exception.hpp
+++ src/Exception.hpp
@@ -5,7 +5,7 @@
 #include <string>
 
 template <class E>
-[[noreturn]] inline void Throw(const E& e) noexcept(false)
+inline void Throw(const E& e) noexcept(false)
 {
     throw e;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,18 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <iostream>
+#include"Window.hpp"
 
 int main() {
-    glfwInit();
+    Vulkan::WindowConfig window_config
+    { 
+        "VulkanRT",
+        800,
+        600,
+        false,
+        false,
+        false
+    };
+    Vulkan::Window window(window_config);
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported" << std::endl;
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
+    window.Run();
 
     return 0;
 }
